a1a3917905095c2037c03fc5a5419304222311b1
BRIAN MUENZENMEYER <brian.muenzenmeyer@gmail.com>
2016-06-11 06:45:30 -0500

    watch meta directory cloese #30

--- gulpfile.js
+++ gulpfile.js
@@ -161,6 +161,7 @@ gulp.task('pl-connect', ['lab'], function() {
     path.resolve(paths().source.data, '*.json'),
     path.resolve(paths().source.fonts + '/*'),
     path.resolve(paths().source.images + '/*'),
+    path.resolve(paths().source.meta, '*'),
     path.resolve(paths().source.annotations + '/*')
   ].concat(getTemplateWatches());
 
