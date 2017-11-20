efbb1f000c86e3183f7a4ab7391cb72b8a32434c
Brian Muenzenmeyer <brian.muenzenmeyer@gmail.com>
2017-11-20 01:05:29 -0600

    feat(gulpfile): distill core bahavior

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,70 @@
 /******************************************************
  * PATTERN LAB NODE
  * EDITION-NODE-GULP
- * The gulp wrapper around patternlab-node core, providing tasks to interact with the core library and move supporting frontend assets.
+ * The gulp wrapper around patternlab-node core, providing tasks to interact with the core library.
 ******************************************************/
-var gulp = require('gulp'),
-  path = require('path'),
-  argv = require('minimist')(process.argv.slice(2)),
-  _ = require('lodash');
-
-/**
- * Normalize all paths to be plain, paths with no leading './',
- * relative to the process root, and with backslashes converted to
- * forward slashes. Should work regardless of how the path was
- * written. Accepts any number of parameters, and passes them along to
- * path.resolve().
- *
- * This is intended to avoid all known limitations of gulp.watch().
- *
- * @param {...string} pathFragment - A directory, filename, or glob.
-*/
-function normalizePath() {
-  return path
-    .relative(
-      process.cwd(),
-      path.resolve.apply(this, arguments)
-    )
-    .replace(/\\/g, "/");
-}
+const gulp = require('gulp');
+const argv = require('minimist')(process.argv.slice(2));
 
 /******************************************************
- * PATTERN LAB CONFIGURATION - API with core library
+ * PATTERN LAB  NODE WRAPPER TASKS with core library
 ******************************************************/
-//read all paths from our namespaced config file
-var config = require('./patternlab-config.json'),
-  patternlab = require('patternlab-node')(config);
+const config = require('./patternlab-config.json');
+const patternlab = require('@pattern-lab/patternlab-node')(config);
 
-function paths() {
-  return config.paths;
+function build() {
+  return patternlab.build({
+    watch: argv.watch,
+    cleanPublic: config.cleanPublic
+  }).then(() =>{
+    // do something else when this promise resolves
+  });
 }
 
-function getConfiguredCleanOption() {
-  return config.cleanPublic;
-}
-
-function getPathsToCopy() {
-  const pathsToCopy = _.cloneDeep(paths());
-  delete pathsToCopy.source.root;
-  delete pathsToCopy.source.patterns;
-  delete pathsToCopy.source.data;
-  delete pathsToCopy.source.meta;
-  delete pathsToCopy.source.annotations;
-  delete pathsToCopy.source.styleguide;
-  delete pathsToCopy.source.patternlabFiles;
-  return pathsToCopy;
-}
-
-/**
- * Performs the actual build step. Accomodates both async and sync
- * versions of Pattern Lab.
- * @param {function} done - Gulp done callback
- */
-function build(done) {
-  //const buildResult = patternlab.build(
-  const buildResult = patternlab.serve(
-    () => {}, {
-      cleanPublic: getConfiguredCleanOption(),
-      watch: true
-    }
-  );
-
-  patternlab.events.on('patternlab-asset-change', (data) => {
-    console.log(126, 'EDITION EVENT! ', data);
-  })
-
-  // handle async version of Pattern Lab
-  if (buildResult instanceof Promise) {
-    return buildResult.then(done);
+function serve() {
+  return patternlab.serve({
+    cleanPublic: config.cleanPublic
   }
-
-  // handle sync version of Pattern Lab
-  done();
-  return null;
+).then(() => {
+  // do something else when this promise resolves
+  });
 }
 
-gulp.task('patternlab:version', function (done) {
+gulp.task('patternlab:version', function () {
   patternlab.version();
-  done();
 });
 
-gulp.task('patternlab:help', function (done) {
+gulp.task('patternlab:help', function () {
   patternlab.help();
-  done();
 });
 
-gulp.task('patternlab:patternsonly', function (done) {
-  patternlab.patternsonly(done, getConfiguredCleanOption());
+gulp.task('patternlab:patternsonly', function () {
+  patternlab.patternsonly(config.cleanPublic);
 });
 
-gulp.task('patternlab:liststarterkits', function (done) {
+gulp.task('patternlab:liststarterkits', function () {
   patternlab.liststarterkits();
-  done();
 });
 
-gulp.task('patternlab:loadstarterkit', function (done) {
+gulp.task('patternlab:loadstarterkit', function () {
   patternlab.loadstarterkit(argv.kit, argv.clean);
-  done();
 });
 
-gulp.task('patternlab:build', gulp.series(build));
+gulp.task('patternlab:build', function () {
+  build().then(() => {
+    // do something else when this promise resolves
+  });
+});
 
-gulp.task('patternlab:installplugin', function (done) {
+gulp.task('patternlab:serve', function () {
+  serve().then(() => {
+    // do something else when this promise resolves
+  });
+});
+
+gulp.task('patternlab:installplugin', function () {
   patternlab.installplugin(argv.plugin);
-  done();
 });
 
-/******************************************************
- * COMPOUND TASKS
-******************************************************/
-gulp.task('default', gulp.series('patternlab:build'));
-gulp.task('patternlab:watch', gulp.series('patternlab:build', watch));
-gulp.task('patternlab:serve', gulp.series('patternlab:build', 'patternlab:connect', watch));
+gulp.task('default', ['patternlab:help']);
+
